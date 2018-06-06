f12ab25d584a9c83c134d8218d890eb0d2a18165
EdwardDunn <edwarddunn12@gmail.com>
2018-06-06 13:17:20 +0100

    created App and Tests packages

---
+++ Tests/TrainingDataTests.py
@@ -0,0 +1,12 @@
+'''
+Created on 6 Jun 2018
+
+@author: Edward Dunn
+'''
+
+import unittest
+
+class TrainingDataTests(unittest.TestCase):
+    
+    def test(self):
+        self.assertEqual(first, second, msg)

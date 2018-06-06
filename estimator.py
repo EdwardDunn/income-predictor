60b71ebf83ef2b8afa697ad1e6c9d4d5f273b1fe
EdwardDunn <edwarddunn12@gmail.com>
2018-06-06 12:31:37 +0100

    added run_estimator method

--- estimator.py
+++ estimator.py
@@ -1,8 +1,19 @@
 '''
 Created on 6 Jun 2018
 
+Description:
+Module used to create a deep neural network using TensorFlow. Functions for cross-validation and
+evaluation included.
+
 @author: Edward Dunn
+
+@version: 1.0.0.0
 '''
+
+import tensorflow as tf
+import os
+import cv2
+
 
 # Number of samples that are to be propagated through the network
 BATCH_SIZE = 200
@@ -64,3 +75,52 @@ def train_model_with_cross_validation():
     
     # Display final accuracy result
     print("\nTesting accuracy: " ,finalAccuracy)
+    
+    
+def run_estimator(testing_rows):
+  
+    global BATCH_SIZE
+    global TRAIN_STEPS
+     
+    print("Training model")
+
+    training_data_path = "trainingset.csv" 
+    testing_data_path = "testingset.csv"
+    
+    # If training and testing data sets already exist, delete them
+    if os.path.exists(training_data_path) and os.path.exists(testing_data_path):
+        os.remove(training_data_path)
+        os.remove(testing_data_path)
+    
+    # Fetch and parse data
+    (trainFeatures, trainLabels), (testFeatures, testLabels) = TrainingData.load_data(testing_rows)
+    
+    # Get each feature from the FeatureLabels module and add it to the featureColumns array
+    featureColumns = []  
+    for feature in get_feature_labels():
+        # Ensure the label name 'emotion' is not used
+        if(feature != 'emotion'):
+            featureColumns.append(tf.feature_column.numeric_column(key=feature))
+    
+    # Using a fully connected neural network (deep neural network)
+    # The file path is specified so that classifier has persistence
+    classifier = tf.estimator.DNNClassifier(
+        feature_columns=featureColumns,
+        # Three hidden layers 
+        hidden_units=[60, 40, 20],
+        # The model must choose between 7 classes.
+        n_classes=7, model_dir=MODEL_DIR)
+    
+    # Call the estimators train function and pass training data through it
+    # Stop training after the specified number of TRAIN_STEPS (this is 1000)
+    classifier.train(
+        input_fn=lambda:TrainingData.train_input_fn(trainFeatures, trainLabels,
+                                                 BATCH_SIZE),
+        steps=TRAIN_STEPS)
+    
+    # Call the estimators evaluate function to evaluate the model
+    evalResult = classifier.evaluate(
+        input_fn=lambda:TrainingData.eval_input_fn(testFeatures, testLabels,
+                                                BATCH_SIZE))
+    
+    return evalResult
